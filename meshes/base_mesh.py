800e830535fad926bd3a0e22791f7ddcab35058c
Prajanya Chauhan <chauhanprajanya28@gmail.com>
2026-06-07 20:54:53 +0530

    Add files via upload

---
+++ meshes/base_mesh.py
@@ -0,0 +1,31 @@
+import numpy as np 
+
+class Basemesh:
+    def __init__(self):
+        #OpenGL context 
+        self.ctx = None
+        # shader program 
+        self.program = None
+        # Here we will define the vertex array for object formation 
+        # Vertex buffer data type : '3f 3f'
+        self.vbo_format = None
+        # attribute names according to the format 
+        self.attrs: tuple[str,...] = None
+        # vertex array object
+        self.vao = None 
+        # we are using a vertex array object as they store the state required to read and feed it to the graphics pipeline, it eliminates the need to repeateadly rebind buffers and configure vertex attributes, they are used becase they help in optimizing performance and keep the code clean, it is a standard procedure to them 
+
+    def get_vertex_data(self) -> np.array:...
+
+    def get_vao(self):
+        vertex_data = self.get_vertex_data()
+        if vertex_data.size == 0:
+            return None
+        vbo = self.ctx.buffer(vertex_data)
+        vao = self.ctx.vertex_array(
+            self.program,[(vbo,self.vbo_format, *self.attrs)], skip_errors = True
+        )
+        return vao
+    def render(self):
+        if self.vao is not None:
+            self.vao.render()
---
+++ meshes/quad_mesh.py
@@ -0,0 +1,27 @@
+from world_objects.settings import * 
+from meshes.base_mesh import Basemesh
+
+class Quadmesh(Basemesh):
+    def __init__(self, app):
+        super().__init__()
+
+        self.app = app
+        self.ctx = app.ctx
+        self.program = app.shader_program.quad 
+
+        self.vbo_format = '3f 3f' #3 floats per vertex co ordinates and 3 floats per color
+        self.attrs = ('in_position', 'in_color')
+        self.vao = self.get_vao()
+
+    def get_vertex_data(self):
+        vertices = [
+            (0.5,0.5,0.0), (-0.5,0.5,0.0), (-0.5,-0.5,0.0),
+            (0.5,0.5,0.0), (-0.5,-0.5,0.0), (0.5,-0.5,0.0)
+        ]
+        colors = [
+         (0,1,0), (1,0,0), (1,1,0),
+         (0,1,0), (1,1,0), (0,0,1)
+        ]
+        # Converting these lists into numpy arrays for mathematical computations 
+        vertex_data = np.hstack([vertices, colors], dtype = 'float32')
+        return vertex_data
